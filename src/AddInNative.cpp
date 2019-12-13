9fad9efabd6b9e28178e596287323abeaeeacd4d
Kandrashin Denis <mail@lintest.ru>
2019-12-13 21:07:23 +0300

    Init repo

---
+++ src/AddInNative.cpp
@@ -0,0 +1,307 @@
+﻿#include "stdafx.h"
+
+#include <minwindef.h>
+#include <wtypes.h>
+#include "AddInNative.h"
+#include "convertor.h"
+#include <memory>
+#include "WinCtrl.h"
+
+static std::wstring param(tVariant* paParams, const long lSizeArray)
+{
+	std::wstring result;
+	switch (TV_VT(paParams))
+	{
+	case VTYPE_PWSTR: {
+		wchar_t* str = 0;
+		::convFromShortWchar(&str, TV_WSTR(paParams));
+		result = str;
+		delete[] str;
+	} break;
+	default:
+		break;
+	}
+	return result;
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// CAddInNative
+//---------------------------------------------------------------------------//
+CAddInNative::CAddInNative()
+{
+	m_iMemory = 0;
+	m_iConnect = 0;
+}
+//---------------------------------------------------------------------------//
+CAddInNative::~CAddInNative()
+{
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::Init(void* pConnection)
+{
+	m_iConnect = (IAddInDefBase*)pConnection;
+	return m_iConnect != NULL;
+}
+//---------------------------------------------------------------------------//
+long CAddInNative::GetInfo()
+{
+	// Component should put supported component technology version 
+	// This component supports 2.0 version
+	return 2000;
+}
+//---------------------------------------------------------------------------//
+void CAddInNative::Done()
+{
+}
+/////////////////////////////////////////////////////////////////////////////
+// ILanguageExtenderBase
+//---------------------------------------------------------------------------//
+bool CAddInNative::RegisterExtensionAs(WCHAR_T** wsExtensionName)
+{
+	return W(L"WindowsControl", wsExtensionName);
+}
+//---------------------------------------------------------------------------//
+long CAddInNative::GetNProps()
+{
+	return ePropLast;
+}
+
+//---------------------------------------------------------------------------//
+long CAddInNative::FindName(const ADDIN_NAMES names, long size, const WCHAR_T* name)
+{
+	for (long i = 0; i < size; i++) {
+		for (long j = 0; j < ALIAS_COUNT; j++) {
+			if (wcsicmp(names[i][j], name) == 0) return i;
+		}
+	}
+	return -1;
+}
+
+//---------------------------------------------------------------------------//
+const WCHAR_T* CAddInNative::GetName(const ADDIN_NAMES names, long size, long lPropNum, long lPropAlias)
+{
+	if (lPropNum >= size) return NULL;
+	if (lPropAlias >= ALIAS_COUNT) return NULL;
+	return W((wchar_t*)names[lPropNum][lPropAlias]);
+}
+
+//---------------------------------------------------------------------------//
+long CAddInNative::FindProp(const WCHAR_T* wsPropName)
+{
+	return FindName(g_PropNames, ePropLast, wsPropName);
+}
+
+//---------------------------------------------------------------------------//
+const WCHAR_T* CAddInNative::W(const wchar_t *str) const
+{
+	WCHAR_T *res = NULL;
+	W(str, &res);
+	return res;
+}
+
+//---------------------------------------------------------------------------//
+BOOL CAddInNative::W(const wchar_t *str, WCHAR_T** res) const
+{
+	if (m_iMemory && str) {
+		size_t size = wcslen(str) + 1;
+		if (m_iMemory->AllocMemory((void**)res, size * sizeof(WCHAR_T))) {
+			::convToShortWchar(res, str, size);
+			return true;
+		}
+	}
+	return false;
+}
+
+//---------------------------------------------------------------------------//
+BOOL CAddInNative::W(const wchar_t *str, tVariant* res) const
+{
+	TV_VT(res) = VTYPE_PWSTR;
+	res->pwstrVal = NULL;
+	res->wstrLen = 0;
+	if (m_iMemory && str) {
+		size_t size = wcslen(str) + 1;
+		if (m_iMemory->AllocMemory((void**)&res->pwstrVal, size * sizeof(WCHAR_T))) {
+			::convToShortWchar((WCHAR_T **)&res->pwstrVal, str, size);
+			res->wstrLen = size;
+			return true;
+		}
+	}
+	return false;
+}
+
+//---------------------------------------------------------------------------//
+BOOL CAddInNative::W(const DWORD &val, tVariant* res) const
+{
+	TV_VT(res) = VTYPE_UI4;
+	res->uintVal = (uint32_t)val;
+	return true;
+}
+
+//---------------------------------------------------------------------------//
+const WCHAR_T* CAddInNative::GetPropName(long lPropNum, long lPropAlias)
+{
+	return GetName(g_PropNames, ePropLast, lPropNum, lPropAlias);
+}
+
+//---------------------------------------------------------------------------//
+bool CAddInNative::GetPropVal(const long lPropNum, tVariant* pvarPropVal)
+{
+	switch (lPropNum) {
+	case eCurrentWindow:
+		return W((DWORD)SetWindow::CurrentWindow(), pvarPropVal);
+	case eActiveWindow:
+		return W((DWORD)SetWindow::ActiveWindow(), pvarPropVal);
+	case eProcessId:
+		return W(SetWindow::ProcessId(), pvarPropVal);
+	default:
+		return false;
+	}
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::SetPropVal(const long lPropNum, tVariant *varPropVal)
+{
+	return false;
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::IsPropReadable(const long lPropNum)
+{
+	return true;
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::IsPropWritable(const long lPropNum)
+{
+	return false;
+}
+//---------------------------------------------------------------------------//
+long CAddInNative::GetNMethods()
+{
+	return eMethLast;
+}
+//---------------------------------------------------------------------------//
+long CAddInNative::FindMethod(const WCHAR_T* wsMethodName)
+{
+	return FindName(g_MethodNames, eMethLast, wsMethodName);
+}
+//---------------------------------------------------------------------------//
+const WCHAR_T* CAddInNative::GetMethodName(const long lMethodNum, const long lMethodAlias)
+{
+	return GetName(g_MethodNames, eMethLast, lMethodNum, lMethodAlias);
+}
+//---------------------------------------------------------------------------//
+long CAddInNative::GetNParams(const long lMethodNum)
+{
+	switch (lMethodNum)
+	{
+	case eSetWindowSize: 
+		return 3;
+	case eSetWindowPos: 
+		return 3;
+	case eActivateWindow:
+		return 1;
+	case eEnableResizing:
+		return 2;
+	case eCaptureWindow:
+		return 1;
+	default:
+		return 0;
+	}
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::GetParamDefValue(const long lMethodNum, const long lParamNum, tVariant *pvarParamDefValue)
+{
+	TV_VT(pvarParamDefValue) = VTYPE_EMPTY;
+	return false;
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::HasRetVal(const long lMethodNum)
+{
+	switch (lMethodNum)
+	{
+	case eGetWindowList:
+	case eSetWindowSize:
+	case eSetWindowPos:
+	case eActivateWindow:
+	case eCaptureWindow:
+		return true;
+	default:
+		return false;
+	}
+}
+//---------------------------------------------------------------------------//
+bool CAddInNative::CallAsProc(const long lMethodNum, tVariant* paParams, const long lSizeArray)
+{
+	switch (lMethodNum)
+	{
+	case eGetWindowList:
+		SetWindow::GetWindowList();
+		return true;
+	case eSetWindowSize:
+		return SetWindow::SetWindowSize(paParams, lSizeArray);
+	case eSetWindowPos:
+		return SetWindow::SetWindowPos(paParams, lSizeArray);
+	case eActivateWindow:
+		return SetWindow::ActivateWindow(paParams, lSizeArray);
+	case eEnableResizing:
+		return SetWindow::EnableResizing(paParams, lSizeArray);
+	default:
+		return false;
+	}
+}
+
+//---------------------------------------------------------------------------//
+bool CAddInNative::CallAsFunc(const long lMethodNum, tVariant* pvarRetValue, tVariant* paParams, const long lSizeArray)
+{
+	switch (lMethodNum) {
+	case eGetWindowList:
+		return W(SetWindow::GetWindowList(), pvarRetValue);
+	case eSetWindowSize:
+		return W(SetWindow::SetWindowSize(paParams, lSizeArray), pvarRetValue);
+	case eSetWindowPos:
+		return W(SetWindow::SetWindowPos(paParams, lSizeArray), pvarRetValue);
+	case eActivateWindow:
+		return W(SetWindow::ActivateWindow(paParams, lSizeArray), pvarRetValue);
+	case eEnableResizing:
+		return W(SetWindow::EnableResizing(paParams, lSizeArray), pvarRetValue);
+	case eCaptureWindow: 
+		return SetWindow(m_iMemory).CaptureWindow(pvarRetValue, paParams, lSizeArray);
+	default:
+		return false;
+	}
+}
+//---------------------------------------------------------------------------//
+void CAddInNative::SetLocale(const WCHAR_T* loc)
+{
+#if !defined( __linux__ ) && !defined(__APPLE__)
+	_wsetlocale(LC_ALL, loc);
+#else
+	//We convert in char* char_locale
+	//also we establish locale
+	//setlocale(LC_ALL, char_locale);
+#endif
+}
+/////////////////////////////////////////////////////////////////////////////
+// LocaleBase
+//---------------------------------------------------------------------------//
+bool CAddInNative::setMemManager(void* mem)
+{
+	m_iMemory = (IMemoryManager*)mem;
+	return m_iMemory != 0;
+}
+//---------------------------------------------------------------------------//
+void CAddInNative::addError(uint32_t wcode, const wchar_t* source, const wchar_t* descriptor, long code)
+{
+	if (m_iConnect)
+	{
+		WCHAR_T *err = 0;
+		WCHAR_T *descr = 0;
+
+		::convToShortWchar(&err, source);
+		::convToShortWchar(&descr, descriptor);
+
+		m_iConnect->AddError(wcode, err, descr, code);
+		delete[] err;
+		delete[] descr;
+	}
+}
+//---------------------------------------------------------------------------//
+
---
+++ src/convertor.cpp
@@ -0,0 +1,88 @@
+#include "stdafx.h"
+#include "convertor.h"
+#include <iomanip>
+#include <sstream>
+#include <vector>
+
+//---------------------------------------------------------------------------//
+uint32_t convToShortWchar(WCHAR_T** Dest, const wchar_t* Source, uint32_t len)
+{
+	if (!len)
+		len = ::wcslen(Source) + 1;
+
+	if (!*Dest)
+		*Dest = new WCHAR_T[len];
+
+	WCHAR_T* tmpShort = *Dest;
+	wchar_t* tmpWChar = (wchar_t*)Source;
+	uint32_t res = 0;
+
+	::memset(*Dest, 0, len * sizeof(WCHAR_T));
+#ifdef __linux__
+	size_t succeed = (size_t)-1;
+	size_t f = len * sizeof(wchar_t), t = len * sizeof(WCHAR_T);
+	const char* fromCode = sizeof(wchar_t) == 2 ? "UTF-16" : "UTF-32";
+	iconv_t cd = iconv_open("UTF-16LE", fromCode);
+	if (cd != (iconv_t)-1)
+	{
+		succeed = iconv(cd, (char**)&tmpWChar, &f, (char**)&tmpShort, &t);
+		iconv_close(cd);
+		if (succeed != (size_t)-1)
+			return (uint32_t)succeed;
+	}
+#endif //__linux__
+	for (; len; --len, ++res, ++tmpWChar, ++tmpShort)
+	{
+		*tmpShort = (WCHAR_T)*tmpWChar;
+	}
+
+	return res;
+}
+//---------------------------------------------------------------------------//
+uint32_t convFromShortWchar(wchar_t** Dest, const WCHAR_T* Source, uint32_t len)
+{
+	if (!len)
+		len = getLenShortWcharStr(Source) + 1;
+
+	if (!*Dest)
+		*Dest = new wchar_t[len];
+
+	wchar_t* tmpWChar = *Dest;
+	WCHAR_T* tmpShort = (WCHAR_T*)Source;
+	uint32_t res = 0;
+
+	::memset(*Dest, 0, len * sizeof(wchar_t));
+#ifdef __linux__
+	size_t succeed = (size_t)-1;
+	const char* fromCode = sizeof(wchar_t) == 2 ? "UTF-16" : "UTF-32";
+	size_t f = len * sizeof(WCHAR_T), t = len * sizeof(wchar_t);
+	iconv_t cd = iconv_open("UTF-32LE", fromCode);
+	if (cd != (iconv_t)-1)
+	{
+		succeed = iconv(cd, (char**)&tmpShort, &f, (char**)&tmpWChar, &t);
+		iconv_close(cd);
+		if (succeed != (size_t)-1)
+			return (uint32_t)succeed;
+	}
+#endif //__linux__
+	for (; len; --len, ++res, ++tmpWChar, ++tmpShort)
+	{
+		*tmpWChar = (wchar_t)*tmpShort;
+	}
+
+	return res;
+}
+//---------------------------------------------------------------------------//
+uint32_t getLenShortWcharStr(const WCHAR_T* Source)
+{
+	uint32_t res = 0;
+	WCHAR_T *tmpShort = (WCHAR_T*)Source;
+
+	while (*tmpShort++)
+		++res;
+
+	return res;
+}
+//---------------------------------------------------------------------------//
+
+
