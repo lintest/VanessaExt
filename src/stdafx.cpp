1e268626abe35f045001f8c03a5b7d68f9c8a06c
Kandrashin Denis <mail@lintest.ru>
2019-12-29 15:04:28 +0300

    Исправлена конвертация строк: MB2WC

--- src/stdafx.cpp
+++ src/stdafx.cpp
@@ -6,23 +6,28 @@
 
 std::wstring MB2WC(const std::string &source)
 {
-    size_t len = source.size();
+    std::string tocode = sizeof(wchar_t) == 4 ? "UTF-32" : "UTF-16";
+    iconv_t cd = iconv_open(tocode.c_str(), "UTF-8");
+    if (cd == (iconv_t)-1) return {};
+
+    size_t ilen = source.size() * sizeof(char);
+    size_t olen = (source.size() + 1) * sizeof(wchar_t);
+    char* ip = const_cast<char*>(source.data());
+    char* outbuf = (char*)malloc(olen);
+    if (outbuf == NULL) { iconv_close(cd); return {}; } 
+
+    char *op = outbuf;
     std::wstring result;
-    result.resize(len + 1);
-
-    char* src = source.data();
-    wchar_t* trg = result.data();
-
-    size_t succeed = (size_t)-1;
-    size_t f = len * sizeof(char), t = len * sizeof(wchar_t);
-    iconv_t cd = iconv_open("UTF-32", "UTF-8");
-    if (cd != (iconv_t)-1) {
-        succeed = iconv(cd, (char**)&src, &f, (char**)&trg, &t);
-        iconv_close(cd);
-        if(succeed != (size_t)-1)
-            return result;
+    size_t icount = ilen;
+    size_t ocount = olen;
+    size_t succeed = iconv(cd, (char**)&ip, &icount, (char**)&op, &ocount); 
+    if (succeed != (size_t)-1) {
+        outbuf[olen - ocount] = '\0';
+        result = (wchar_t*)outbuf;
     }
-    return {};
+    free(outbuf); 
+    iconv_close(cd);
+    return result;
 }
 
 std::string WC2MB(const std::wstring &source)
@@ -44,6 +49,35 @@ std::string WC2MB(const std::wstring &source)
             return result;
     }
     return {};
+}
+
+std::string iconv_recode(std::string from, std::string to, std::string text)
+{
+    iconv_t cnv = iconv_open(to.c_str(), from.c_str());
+    if (cnv == (iconv_t)-1) {
+        iconv_close(cnv);
+        return "";
+    }
+
+    char *outbuf;
+    if ((outbuf = (char *)malloc(text.length()*2 + 1)) == NULL) {
+        iconv_close(cnv);
+        return "";
+    }
+
+    char *ip = (char*) text.c_str(), *op = outbuf;
+    size_t icount = text.length(), ocount = text.length()*2;
+
+    if (iconv(cnv, &ip, &icount, &op, &ocount) != (size_t) - 1) {
+        outbuf[text.length()*2 - ocount] = '\0';
+        text = outbuf;
+    } else {
+        text = "";
+    }
+
+    free(outbuf);
+    iconv_close(cnv);
+    return text;
 }
 
 #else//__linux__
