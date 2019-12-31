4a69d74cc161cbde894927c9294ffb2960e011ac
Kandrashin Denis <mail@lintest.ru>
2019-12-31 17:02:02 +0300

    Скрипт для сборки Ubuntu

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-#build script for CentOS 5
-
 PATH=~/bin:$PATH
 set -e
 
@@ -9,41 +7,60 @@ export SCL=`which scl`
 if [ ! -z $SCL ]; then
     export HAS_DTS=`scl -l | grep devtoolset-2`
 fi
-cmake -E echo "Build 32" ; 
-if [ ! -d build32 ]; then
-    cmake -E make_directory build32
-    pushd build32
-    if [ "${SCL}" != "" -a "${HAS_DTS}" != "" ]; then
-        scl enable devtoolset-2 'cmake -D TARGET_PLATFORM_32:BOOL=ON  -D CMAKE_BUILD_TYPE:STRING=RelWithDebInfo --build .. ';
-    else
-        cmake -D CMAKE_BUILD_TYPE:STRING=RelWithDebInfo -D TARGET_PLATFORM_32:BOOL=ON --build .. ;
+
+if [ -n "$1" ]; then
+    if [ $1 -eq 32 ]; then
+        build32=1
+        build64=0
     fi
-    popd
-fi ;
-pushd build32
-cmake --build .
-popd 
-cmake -E remove_directory build32
-
-cmake -E echo "Build 64" ; 
-if [ ! -d build64 ]; then
-    cmake -E make_directory build64
-    pushd build64 ;
-    if [ "${SCL}" != "" -a "${HAS_DTS}" != "" ]; then
-        scl enable devtoolset-2 'cmake -D CMAKE_BUILD_TYPE:STRING=RelWithDebInfo -D TARGET_PLATFORM_32:BOOL=OFF --build .. ';
-    else
-        cmake -D CMAKE_BUILD_TYPE:STRING=RelWithDebInfo -D TARGET_PLATFORM_32:BOOL=OFF --build .. ;
+    if [ $1 -eq 64 ]; then
+        build32=0
+        build64=1
     fi
-    popd
-fi ;
-pushd build64
-cmake --build .
-popd
-cmake -E remove_directory build64
+else
+    build32=1
+    build64=1
+fi
 
-#strip -s bin/*
-pushd bin
-rm -f *.{debug,a}
-for i in *; do eu-strip -f $i.debug $i ; done
-popd
+if [ $build32 -eq 1 ]; then
+    cmake -E echo "Build 32" 
+    if [ ! -d build32 ]; then
+        cmake -E make_directory build32
+        cd build32
+        if [ "${SCL}" != "" -a "${HAS_DTS}" != "" ]; then
+            scl enable devtoolset-2 'cmake -D CMAKE_BUILD_TYPE:STRING=RelWithDebInfo -D TARGET_PLATFORM_32:BOOL=ON --build .. '
+        else
+            cmake -D CMAKE_BUILD_TYPE:STRING=RelWithDebInfo -D TARGET_PLATFORM_32:BOOL=ON --build ..
+        fi
+        cd .. 
+    fi 
+    cd build32 
+    cmake --build .
+    cd ..
+fi    
 
+if [ $build64 -eq 1 ]; then
+    cmake -E echo "Build 64"
+    if [ ! -d build64 ]; then
+        cmake -E make_directory build64
+        cd build64
+        if [ "${SCL}" != "" -a "${HAS_DTS}" != "" ]; then
+            scl enable devtoolset-2 'cmake -D CMAKE_BUILD_TYPE:STRING=RelWithDebInfo -D TARGET_PLATFORM_32:BOOL=OFF --build .. '
+        else
+            cmake -D CMAKE_BUILD_TYPE:STRING=RelWithDebInfo -D TARGET_PLATFORM_32:BOOL=OFF --build ..
+        fi
+        cd ..
+    fi ;
+    cd build64
+    cmake --build .
+    cd ..
+fi    
+
+if [ -z $1 ]; then
+    cmake -E remove_directory build32
+    cmake -E remove_directory build64
+    strip -s bin/*
+    rm -f *.{debug,a}
+fi
+
+cd ..
