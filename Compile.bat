ed2d9e0028c63ee5364123ccf84f2d8fe643e879
Kandrashin Denis <mail@lintest.ru>
2020-01-11 16:48:53 +0300

    Сборка пакета с учётом номера версии

--- Compile.bat
+++ Compile.bat
@@ -1,11 +1,8 @@
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" SetWindow.sln /property:Configuration=Release /property:Platform=x86
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\MSBuild\Current\Bin\MSBuild.exe" SetWindow.sln /property:Configuration=Release /property:Platform=x64
 
-powershell Compress-Archive -LiteralPath .\src\MANIFEST.XML -DestinationPath ./AddIn.zip -Update
-powershell Compress-Archive -LiteralPath .\bin\lib1cWinCtrlLin32.so -DestinationPath ./AddIn.zip -Update
-powershell Compress-Archive -LiteralPath .\bin\lib1cWinCtrlLin64.so -DestinationPath ./AddIn.zip -Update
-powershell Compress-Archive -LiteralPath .\bin32\lib1cWinCtrlWin32.dll -DestinationPath ./AddIn.zip -Update
-powershell Compress-Archive -LiteralPath .\bin64\lib1cWinCtrlWin64.dll -DestinationPath ./AddIn.zip -Update
+oscript .\MakePack.os
+
 copy /b .\AddIn.zip .\Example\Templates\SetWindow\Ext\Template.bin
 
 oscript .\tools\Compile.os .\
