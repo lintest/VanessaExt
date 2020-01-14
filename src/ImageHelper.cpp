f4235116f53729fed6c7ceac3506b803f2e07487
Kandrashin Denis <mail@lintest.ru>
2020-01-14 20:31:05 +0300

    Version 0.06.2

---
+++ src/ImageHelper.cpp
@@ -0,0 +1,93 @@
+#include "ImageHelper.h"
+
+#include <gdiplus.h>
+#pragma comment(lib, "Gdiplus.lib")
+
+class GgiPlusToken {
+private:
+	ULONG_PTR h = NULL;
+public:
+	GgiPlusToken() noexcept {}
+	~GgiPlusToken() { if (h) Gdiplus::GdiplusShutdown(h); }
+	ULONG_PTR* operator &() noexcept { return &h; }
+	BOOL operator!() noexcept { return !h; }
+};
+
+static GgiPlusToken gdiplusToken;
+
+int GetEncoderClsid(const WCHAR* format, CLSID* pClsid)
+{
+	UINT  num = 0;          // number of image encoders
+	UINT  size = 0;         // size of the image encoder array in bytes
+
+	Gdiplus::ImageCodecInfo* pImageCodecInfo = NULL;
+
+	Gdiplus::GetImageEncodersSize(&num, &size);
+	if (size == 0) return -1;  // Failure
+
+	pImageCodecInfo = (Gdiplus::ImageCodecInfo*)(malloc(size));
+	if (pImageCodecInfo == NULL) return -1;  // Failure
+
+	Gdiplus::GetImageEncoders(num, size, pImageCodecInfo);
+
+	for (UINT j = 0; j < num; ++j)
+	{
+		if (wcscmp(pImageCodecInfo[j].MimeType, format) == 0)
+		{
+			*pClsid = pImageCodecInfo[j].Clsid;
+			free(pImageCodecInfo);
+			return j;  // Success
+		}
+	}
+
+	free(pImageCodecInfo);
+	return -1;  // Failure
+}
+
+BOOL ImageHelper::SaveBitmap(HBITMAP hBitmap, tVariant* pvarRetValue)
+{
+	BOOL Ret = FALSE;
+	Gdiplus::Status status = Gdiplus::Ok;
+	if (!gdiplusToken) // initialization of gdi+
+	{
+		const Gdiplus::GdiplusStartupInput input;
+		status = Gdiplus::GdiplusStartup(&gdiplusToken, &input, NULL);
+		if (status != Gdiplus::Ok) return FALSE;
+	}
+
+	CLSID clsid;
+	GetEncoderClsid(L"image/png", &clsid); // retrieving JPEG encoder CLSID
+
+	Gdiplus::Bitmap SrcBitmap(hBitmap, 0); // creating bitmap
+
+	IStream* pStream;
+	if (SUCCEEDED(CreateStreamOnHGlobal(0, TRUE, &pStream))) // creating stream
+	{
+		status = SrcBitmap.Save(pStream, &clsid, 0); // saving image to the stream
+		if (status == Gdiplus::Ok)
+		{
+			LARGE_INTEGER lOfs;
+			ULARGE_INTEGER lSize;
+			lOfs.QuadPart = 0;
+			if (SUCCEEDED(pStream->Seek(lOfs, STREAM_SEEK_END, &lSize))) // retrieving size of stream data (seek to end)
+			{
+				lOfs.QuadPart = 0;
+				if (SUCCEEDED(pStream->Seek(lOfs, STREAM_SEEK_SET, 0))) // seeking to beginning of the stream data
+				{
+					pvarRetValue->strLen = (ULONG)((DWORD_PTR)lSize.QuadPart);
+					m_iMemory->AllocMemory((void**)&pvarRetValue->pstrVal, pvarRetValue->strLen);
+					TV_VT(pvarRetValue) = VTYPE_BLOB;
+					if (pvarRetValue->pstrVal)
+					{
+						if (SUCCEEDED(pStream->Read(pvarRetValue->pstrVal, pvarRetValue->strLen, 0))) // reading stream to buffer
+						{
+							Ret = TRUE;
+						}
+					}
+				}
+			}
+		}
+		pStream->Release(); // releasing stream
+	}
+	return Ret;
+}
