76fbb3ef72ebef5788c7013405f4ae293619778a
Kandrashin Denis <mail@lintest.ru>
2020-03-27 22:38:40 +0300

    Обработка ошибок WebSocket

--- src/WebSocket.cpp
+++ src/WebSocket.cpp
@@ -19,6 +19,34 @@ namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
 namespace net = boost::asio;            // from <boost/asio.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
+#ifdef _WINDOWS
+std::string cp1251_to_utf8(const char* str) {
+	std::string res;
+	int result_u, result_c;
+	result_u = MultiByteToWideChar(1251, 0, str, -1, 0, 0);
+	if (!result_u) { return 0; }
+	wchar_t* ures = new wchar_t[result_u];
+	if (!MultiByteToWideChar(1251, 0, str, -1, ures, result_u)) {
+		delete[] ures;
+		return 0;
+	}
+	result_c = WideCharToMultiByte(65001, 0, ures, -1, 0, 0, 0, 0);
+	if (!result_c) {
+		delete[] ures;
+		return 0;
+	}
+	char* cres = new char[result_c];
+	if (!WideCharToMultiByte(65001, 0, ures, -1, cres, result_c, 0, 0)) {
+		delete[] cres;
+		return 0;
+	}
+	delete[] ures;
+	res.append(cres);
+	delete[] cres;
+	return res;
+}
+#endif
+
 // Sends a WebSocket message 
 bool doWebSocket(std::string& url, std::string& msg, std::string& res)
 {
@@ -78,7 +106,7 @@ bool doWebSocket(std::string& url, std::string& msg, std::string& res)
 	catch (std::exception const& e)
 	{
 		std::stringstream ss;
-		ss << "Error: " << e.what();
+		ss << "Error: " << cp1251_to_utf8(e.what());
 		res = ss.str();
 		return false;
 	}
