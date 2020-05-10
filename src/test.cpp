fd580dd0353261c4329dc06b0b579135ce16e750
Kandrashin Denis <mail@lintest.ru>
2020-05-10 17:54:51 +0300

    Исправлена ошибка получения размеров дисплея Linux

--- src/test.cpp
+++ src/test.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <types.h>
 #include "ClipMngr.h"
+#include "ProcMngr.h"
 #include "ScreenMngr.h"
 #include "WindowMngr.h"
 
@@ -17,15 +18,6 @@ int main() {
     paParam.intVal = 44040523;
     paParam.vt = VTYPE_I4;
 
-    json = ScreenManager::GetDisplayList(NULL, 0);
-    std::wcout << std::endl << json << std::endl << std::endl;
-
-    tVariant pVarTrue;
-    pVarTrue.intVal = 1;
-    pVarTrue.vt = VTYPE_BOOL;
-
-    json = ProcessManager::GetProcessList(&pVarTrue, 1);
-    std::wcout << std::endl << json << std::endl << std::endl;
 
     tVariant pVarClient;
     pVarClient.intVal = 48000;
@@ -34,64 +26,27 @@ int main() {
     json = ProcessManager::FindTestClient(&pVarClient, 1);
     std::wstring text = json;
     std::wcout << std::endl << json << std::endl << std::endl;
+*/
+    tVariant pVarTrue;
+    pVarTrue.intVal = 1;
+    pVarTrue.vt = VTYPE_BOOL;
+    json = ProcessManager::GetProcessList(&pVarTrue, 1);
+    std::wcout << L"GetProcessList";
+    std::wcout << std::endl << json << std::endl << std::endl;
 
     tVariant pVarProc;
-    pVarProc.intVal = 4792;
+    pVarProc.intVal = 0;
     pVarProc.vt = VTYPE_I4;
-
     json = WindowManager().GetWindowList(&pVarProc, 1);
+    std::wcout << L"GetWindowList";
     std::wcout << std::endl << json << std::endl << std::endl;
 
     json = ScreenManager::GetScreenList();
+    std::wcout << L"GetScreenList";
     std::wcout << std::endl << json << std::endl << std::endl;
 
     json = ScreenManager::GetDisplayList(NULL, 0);
     std::wcout << std::endl << json << std::endl << std::endl;
 
-    json = WindowManager::GetWindowSize(NULL, 0);
-    std::wcout << std::endl << json << std::endl << std::endl;
-
-    tVariant pVarActive;
-    pVarActive.intVal = (int)WindowManager::ActiveWindow();
-    pVarActive.vt = VTYPE_I4;
-
-    json = ScreenManager::GetScreenInfo();
-    std::wcout << std::endl << json << std::endl << std::endl;
-
-    json = ScreenManager::GetDisplayList(&pVarActive, 1);
-    std::wcout << std::endl << json << std::endl << std::endl;
-
-    json = ScreenManager::GetDisplayInfo(&pVarActive, 1);
-    std::wcout << std::endl << json << std::endl << std::endl;
-
-    json = WindowManager::GetWindowInfo(NULL, 0);
-    std::wcout << std::endl << json << std::endl << std::endl;
-
-    json = ClipboardManager(nullptr).GetFiles();
-    std::wcout << std::endl << json << std::endl << std::endl;
-
-    clip::image image;
-    clip::get_image(image);
-
-    std::string new_content = "test";
-    clip::set_text(new_content);
-
-    std::string clipboard;
-    clip::get_text(clipboard);
-    std::cout << "'" << clipboard << "' copied\n";
-
-    json = ClipboardManager(nullptr).GetFiles();
-    std::wcout << std::endl << json << std::endl << std::endl;
-
-    std::wcout << ScreenManager::GetCursorPos() << std::endl;
-
-    tVariant pVarActive[2];
-    pVarActive[0].intVal = 800;
-    pVarActive[0].vt = VTYPE_I4;
-    pVarActive[1].intVal = 800;
-    pVarActive[1].vt = VTYPE_I4;
-
-    ScreenManager::SetCursorPos(&pVarActive[0], 2);
-*/
     return 0;
 }
