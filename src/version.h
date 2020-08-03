c6e9553cd00202c38fe3ed04ff80e4740fe00cbc
Kandrashin Denis <mail@lintest.ru>
2020-08-04 00:14:27 +0300

    Версия 1.0.10

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               0
-#define VERSION_REVISION            9
+#define VERSION_REVISION           10
 #define VERSION_BUILD               0
 
 #define STRINGIZE2(s) #s
