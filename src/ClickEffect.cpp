13408c4ebc68f2d9a0985ef91dfb9a9d61ddb299
Kandrashin Denis <mail@lintest.ru>
2020-09-03 19:35:47 +0300

    Оптимизация кода видеоэффекта нажатия мыши

--- src/ClickEffect.cpp
+++ src/ClickEffect.cpp
@@ -4,24 +4,28 @@
 #include "windows.h"
 #include "ClickEffect.h"
 
-class ClickEffect::ClickData {
+class ClickEffect::Settings {
 public:
-	bool last = false;
-	COLORREF color = RGB(200, 50, 50);
-	int radius = 30;
-	int width = 12;
-	int delay = 12;
-	int trans = 127;
-	int step = 0;
-	int max = 0;
-public:
-	static ClickData settings;
-	ClickData() {}
-	ClickData(int64_t color, int64_t radius, int64_t width, int64_t delay, int64_t trans)
-		: color((COLORREF)color), radius((int)radius), width((int)width), delay((int)delay), trans((int)trans){}
-	ClickData(const ClickData& d)
-		: color((COLORREF)d.color), radius((int)d.radius), width((int)d.width), delay((int)d.delay), trans((int)d.trans) {}
-	ClickData& operator=(const ClickData& d) {
+	static Settings settings;
+
+	Settings() {}
+
+	~Settings();
+
+	Settings(
+		int64_t color, 
+		int64_t radius, 
+		int64_t width, 
+		int64_t delay, 
+		int64_t trans
+	): 
+		color((COLORREF)color), 
+		radius((int)radius), 
+		width((int)width), 
+		delay((int)delay), 
+		trans((int)trans) {}
+
+	Settings& operator=(const Settings& d) {
 		color = d.color;
 		radius = d.radius;
 		width = d.width;
@@ -29,44 +33,84 @@ public:
 		trans = d.trans;
 		return *this;
 	}
-	HPEN CreatePen();
+private:
+	COLORREF color = RGB(200, 50, 50);
+	int radius = 30;
+	int width = 12;
+	int delay = 12;
+	int trans = 127;
+	friend Painter;
+};
+
+class ClickEffect::Painter {
+private:
+	const COLORREF color;
+	const int radius;
+	const int width;
+	const int delay;
+	const int trans;
+	bool last = false;
+	int limit = 0;
+	int step = 0;
+public:
+	Painter(const Settings& s) :
+		color(s.color),
+		radius(s.radius),
+		width(s.width),
+		delay(s.delay),
+		trans(s.trans),
+		limit(s.radius) {}
+	LRESULT Paint(HWND hWnd);
+	void Create();
 };
 
 #define ID_CLICK_TIMER 1
 
-HPEN ClickEffect::ClickData::CreatePen()
+HHOOK ClickEffect::hMouseHook = NULL;
+
+ClickEffect::Settings ClickEffect::Settings::settings;
+
+ClickEffect::Settings::~Settings()
 {
-	int w = width * (max - step) / max;
-	return ::CreatePen(PS_SOLID, w, color);
+	ClickEffect::Unhook();
 }
 
-LRESULT CALLBACK PaintWindow(HWND hWnd)
+ClickEffect::Painter* ClickEffect::painter(HWND hWnd)
 {
-	auto data = (ClickEffect::ClickData*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
-	HPEN pen = data->CreatePen();
+	return (ClickEffect::Painter*)GetWindowLongPtr(hWnd, GWLP_USERDATA);
+}
+
+ClickEffect::Painter* ClickEffect::painter(LPVOID lpParam)
+{
+	return (ClickEffect::Painter*)lpParam;
+}
+
+LRESULT ClickEffect::Painter::Paint(HWND hWnd)
+{
 	PAINTSTRUCT ps;
 	HDC hdc = BeginPaint(hWnd, &ps);
+	int w = width * (limit - step) / limit;
+	HPEN pen = CreatePen(PS_SOLID, w, color);
 	HPEN hOldPen = (HPEN)SelectObject(hdc, pen);
 	HBRUSH hOldBrush = (HBRUSH)SelectObject(hdc, GetStockObject(BLACK_BRUSH));
-	int x = data->radius - data->step;
-	int d = data->radius + data->step;
+	int x = radius - step;
+	int d = radius + step;
 	Ellipse(hdc, x, x, d, d);
 	SelectObject(hdc, hOldBrush);
 	SelectObject(hdc, hOldPen);
 	EndPaint(hWnd, &ps);
 	DeleteObject(pen);
-	data->step++;
-	if (data->step > data->max) {
-		if (data->last) {
+	step++;
+	if (step > limit) {
+		KillTimer(hWnd, ID_CLICK_TIMER);
+		if (last) {
 			SendMessage(hWnd, WM_DESTROY, 0, 0);
-			KillTimer(hWnd, ID_CLICK_TIMER);
 		}
 		else {
-			KillTimer(hWnd, ID_CLICK_TIMER);
-			SetTimer(hWnd, ID_CLICK_TIMER, data->delay * 2, NULL);
-			data->max = data->radius / 2;
-			data->last = true;
-			data->step = 0;
+			SetTimer(hWnd, ID_CLICK_TIMER, delay * 2, NULL);
+			limit = radius / 2;
+			last = true;
+			step = 0;
 		}
 	}
 	return 0L;
@@ -89,7 +133,7 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 		}
 		return 0;
 	case WM_PAINT:
-		return PaintWindow(hWnd);
+		return ClickEffect::painter(hWnd)->Paint(hWnd);
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		return 0;
@@ -98,14 +142,11 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	}
 }
 
-DWORD WINAPI ThreadProc(LPVOID lpParam)
+void ClickEffect::Painter::Create()
 {
-	auto data = (ClickEffect::ClickData*)lpParam;
-	data->max = data->radius;
-
 	POINT p;
 	::GetCursorPos(&p);
-	int r = data->radius;
+	int r = radius;
 	int x = p.x - r - 1;
 	int y = p.y - r - 1;
 	int w = r * 2 + 2;
@@ -128,52 +169,55 @@ DWORD WINAPI ThreadProc(LPVOID lpParam)
 	DWORD dwStyle = WS_OVERLAPPED;
 	DWORD dwExStyle = WS_EX_LAYERED | WS_EX_TOPMOST | WS_EX_TOOLWINDOW;
 	HWND hWnd = CreateWindowEx(dwExStyle, name, name, dwStyle, x, y, w, h, 0, 0, hModule, 0);
-	SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)lpParam);
+	SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)this);
 
-	MSG msg;
-	SetTimer(hWnd, ID_CLICK_TIMER, data->delay, NULL);
+	SetTimer(hWnd, ID_CLICK_TIMER, delay, NULL);
 	ShowWindow(hWnd, SW_SHOWNOACTIVATE);
 	UpdateWindow(hWnd);
-	SetLayeredWindowAttributes(hWnd, RGB(0, 0, 0), data->trans, LWA_COLORKEY | LWA_ALPHA);
+	SetLayeredWindowAttributes(hWnd, RGB(0, 0, 0), trans, LWA_COLORKEY | LWA_ALPHA);
+}
+
+DWORD WINAPI ThreadProc(LPVOID lpParam)
+{
+	auto painter = ClickEffect::painter(lpParam);
+	painter->Create();
+	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-	delete data;
+	delete painter;
 	return 0;
 }
-
-ClickEffect::ClickData ClickEffect::ClickData::settings;
-HHOOK hMouseHook = NULL;
 
 LRESULT CALLBACK HookProc(int code, WPARAM wParam, LPARAM lParam)
 {
 	if (code >= 0) {
 		switch (wParam) {
 		case WM_RBUTTONDOWN:
-		case WM_LBUTTONDOWN: 
+		case WM_LBUTTONDOWN:
 			ClickEffect::Show();
 		}
 	}
-	return CallNextHookEx(hMouseHook, code, wParam, lParam);
+	return CallNextHookEx(ClickEffect::hMouseHook, code, wParam, lParam);
 }
 
 void ClickEffect::Show()
 {
-	ClickData* data = new ClickData(ClickData::settings);
-	CreateThread(0, NULL, ThreadProc, (LPVOID)data, NULL, NULL);
+	Painter* painter = new Painter(Settings::settings);
+	CreateThread(0, NULL, ThreadProc, (LPVOID)painter, NULL, NULL);
 }
 
 void ClickEffect::Show(int64_t color, int64_t radius, int64_t width, int64_t delay, int64_t trans)
 {
-	ClickData* data = new ClickData(color, radius, width, delay, trans);
-	CreateThread(0, NULL, ThreadProc, (LPVOID)data, NULL, NULL);
+	Painter* painter = new Painter(Settings(color, radius, width, delay, trans));
+	CreateThread(0, NULL, ThreadProc, (LPVOID)painter, NULL, NULL);
 }
 
 void ClickEffect::Hook(int64_t color, int64_t radius, int64_t width, int64_t delay, int64_t trans)
 {
 	if (hMouseHook) UnhookWindowsHookEx(hMouseHook);
-	ClickData::settings = ClickData(color, radius, width, delay, trans);
+	Settings::settings = Settings(color, radius, width, delay, trans);
 	hMouseHook = SetWindowsHookEx(WH_MOUSE, &HookProc, hModule, NULL);
 }
 
@@ -182,6 +226,5 @@ void ClickEffect::Unhook()
 	if (hMouseHook) UnhookWindowsHookEx(hMouseHook);
 	hMouseHook = NULL;
 }
-
 
 #endif //_WINDOWS
--- src/ClickEffect.h
+++ src/ClickEffect.h
@@ -6,7 +6,11 @@
 
 class ClickEffect {
 public:
-	class ClickData;
+	class Painter;
+	class Settings;
+	static HHOOK hMouseHook;
+	static Painter* painter(HWND hWnd);
+	static Painter* painter(LPVOID lpParam);
 	static void Hook(int64_t color, int64_t radius, int64_t width, int64_t delay, int64_t trans);
 	static void Show(int64_t color, int64_t radius, int64_t width, int64_t delay, int64_t trans);
 	static void Show();
