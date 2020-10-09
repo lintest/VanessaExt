e6cea2abed0f139834cc4d87323bda104630ef59
Kandrashin Denis <mail@lintest.ru>
2020-10-09 20:15:56 +0300

    Автоматизированная сборка AppVeyor

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 #define VER_FILENAME VanessaExt
-#define VERSION_FULL 1.1.10.0
+#define VERSION_FULL 1.2.1.0
 #define VERSION_MAJOR     1
-#define VERSION_MINOR     1
-#define VERSION_REVISION  10
+#define VERSION_MINOR     2
+#define VERSION_REVISION  1
 #define VERSION_BUILD     0
