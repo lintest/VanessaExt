16359122793a8a8bc8fe42b946002c1f3b092e0e
Kandrashin Denis <mail@lintest.ru>
2020-10-15 19:59:15 +0300

    Порт для вызова Build Worker API

--- appveyor.ps1
+++ appveyor.ps1
@@ -46,3 +46,4 @@ if (!(Test-Path $dist1c)) {
   Write-Host "Download 1cv8t.7z ..."
   Invoke-WebRequest -Uri $url1c -OutFile $dist1c
 }
+Set-Content -Path app_port.txt -Value ([uri] $env:APPVEYOR_API_URL).Port
