e8763133c2f74fbc95c820642bf164a69c9afa24
lintest <mail@lintest.ru>
2020-11-23 16:28:28 +0300

    Удалён неиспользуемый код

--- src/FindImage.cpp
+++ src/FindImage.cpp
@@ -29,9 +29,9 @@ std::string BaseHelper::ImageFinder::find(VH picture, VH fragment, int match_met
     matchTemplate(img, templ, result, match_method);
     normalize(result, result, 0, 1, NORM_MINMAX, -1, Mat());
 
-    /// Localizing the best match with minMaxLoc
-    double minVal; double maxVal; Point minLoc; Point maxLoc;
-    minMaxLoc(result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+    /// Localizing the best match
+    Point minLoc; Point maxLoc;
+    minMaxLoc(result, nullptr, nullptr, &minLoc, &maxLoc, Mat());
     /// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
     Point matchLoc = (match_method == TM_SQDIFF || match_method == TM_SQDIFF_NORMED) ? minLoc : maxLoc;
 
