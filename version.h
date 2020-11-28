4664c776e0776354c4af8e2f8d290673b7b94d45
Kandrashin Denis <mail@lintest.ru>
2020-11-28 22:13:54 +0300

    Примеры рисования фигур

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 #define VER_FILENAME VanessaExt
-#define VERSION_FULL 1.2.2.0
+#define VERSION_FULL 1.2.3.0
 #define VERSION_MAJOR     1
 #define VERSION_MINOR     2
-#define VERSION_REVISION  2
+#define VERSION_REVISION  3
 #define VERSION_BUILD     0
