c4d3d9a6ccecac0db9bb8be55d0e0824861e48a1
Kandrashin Denis <mail@lintest.ru>
2021-05-27 21:59:43 +0300

    Тип элемента UIAutomation

--- src/WindowsControl.cpp
+++ src/WindowsControl.cpp
@@ -431,19 +431,79 @@ public:
 
 #include <sstream>
 
+#include <UIAutomationClient.h>
+
+std::string type2str(CONTROLTYPEID typeId) {
+	switch (typeId) {
+	case UIA_ButtonControlTypeId: return "Button";
+	case UIA_CalendarControlTypeId: return "Calendar";
+	case UIA_CheckBoxControlTypeId: return "CheckBox";
+	case UIA_ComboBoxControlTypeId: return "ComboBox";
+	case UIA_EditControlTypeId: return "Edit";
+	case UIA_HyperlinkControlTypeId: return "Hyperlink";
+	case UIA_ImageControlTypeId: return "Image";
+	case UIA_ListItemControlTypeId: return "ListItem";
+	case UIA_ListControlTypeId: return "List";
+	case UIA_MenuControlTypeId: return "Menu";
+	case UIA_MenuBarControlTypeId: return "MenuBar";
+	case UIA_MenuItemControlTypeId: return "MenuItem";
+	case UIA_ProgressBarControlTypeId: return "ProgressBar";
+	case UIA_RadioButtonControlTypeId: return "RadioButton";
+	case UIA_ScrollBarControlTypeId: return "ScrollBar";
+	case UIA_SliderControlTypeId: return "Slider";
+	case UIA_SpinnerControlTypeId: return "Spinner";
+	case UIA_StatusBarControlTypeId: return "StatusBar";
+	case UIA_TabControlTypeId: return "Tab";
+	case UIA_TabItemControlTypeId: return "TabItem";
+	case UIA_TextControlTypeId: return "Text";
+	case UIA_ToolBarControlTypeId: return "ToolBar";
+	case UIA_ToolTipControlTypeId: return "ToolTip";
+	case UIA_TreeControlTypeId: return "Tree";
+	case UIA_TreeItemControlTypeId: return "TreeItem";
+	case UIA_CustomControlTypeId: return "Custom";
+	case UIA_GroupControlTypeId: return "Group";
+	case UIA_ThumbControlTypeId: return "Thumb";
+	case UIA_DataGridControlTypeId: return "DataGrid";
+	case UIA_DataItemControlTypeId: return "DataItem";
+	case UIA_DocumentControlTypeId: return "Document";
+	case UIA_SplitButtonControlTypeId: return "SplitButton";
+	case UIA_WindowControlTypeId: return "Window";
+	case UIA_PaneControlTypeId: return "Pane";
+	case UIA_HeaderControlTypeId: return "Header";
+	case UIA_HeaderItemControlTypeId: return "HeaderItem";
+	case UIA_TableControlTypeId: return "Table";
+	case UIA_TitleBarControlTypeId: return "TitleBar";
+	case UIA_SeparatorControlTypeId: return "Separator";
+	case UIA_SemanticZoomControlTypeId: return "SemanticZoom";
+	case UIA_AppBarControlTypeId: return "AppBar";
+	default: return {};
+	}
+}
+
+
 JSON WindowsControl::info(IUIAutomationElement* element)
 {
 	if (element == nullptr) return {};
 	JSON json;
 
 	UIString name;
-	if (element && SUCCEEDED(element->get_CurrentName(&name))) {
+	if (SUCCEEDED(element->get_CurrentName(&name))) {
 		json["name"] = WC2MB(name);
+	}
+
+	CONTROLTYPEID typeId;
+	if (SUCCEEDED(element->get_CurrentControlType(&typeId))) {
+		json["type"] = type2str(typeId);
+	}
+
+	UIString typeName;
+	if (SUCCEEDED(element->get_CurrentLocalizedControlType(&typeName))) {
+		json["info"] = WC2MB(typeName);
 	}
 
 	std::stringstream ss;
 	SAFEARRAY* id = nullptr;
-	if (element && SUCCEEDED(element->GetRuntimeId(&id))) {
+	if (SUCCEEDED(element->GetRuntimeId(&id))) {
 		void* pVoid = 0;
 		::SafeArrayAccessData(id, &pVoid);
 		const long* pLongs = reinterpret_cast<long*>(pVoid);
@@ -457,7 +517,7 @@ JSON WindowsControl::info(IUIAutomationElement* element)
 	json["id"] = ss.str();
 
 	RECT rect;
-	if (element && SUCCEEDED(element->get_CurrentBoundingRectangle(&rect))) {
+	if (SUCCEEDED(element->get_CurrentBoundingRectangle(&rect))) {
 		json["size"] = {
 			{"left", rect.left },
 			{"top", rect.top },
